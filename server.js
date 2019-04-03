6b40e47e23ef2cb9679a5454b716bbb28e2ad85c
David Weid <david.weid.2@gmail.com>
2019-04-02 22:49:13 -0400

    Update to port config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var mongoose = require("mongoose");
 var db = require("./models");
 
 // Set up local port
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
